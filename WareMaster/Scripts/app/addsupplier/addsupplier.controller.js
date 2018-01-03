aee7bba6062370c5a26f9980bab404e5df45331b
Kreso <kreso@dump.hr>
2018-01-03 14:28:45 +0100

    Minor code refactors

--- WareMaster/Scripts/app/addsupplier/addsupplier.controller.js
+++ WareMaster/Scripts/app/addsupplier/addsupplier.controller.js
@@ -1,8 +1,9 @@
 ﻿angular.module('app').controller('AddSupplierController',
     function ($scope, $state, suppliersRepository, productsRepository, loginRepository) {
         $scope.productsSelected = [];
+        const companyId = loginRepository.getCompanyId();
 
-        productsRepository.getAllProducts(loginRepository.getCompanyId()).then(function (allProducts) {
+        productsRepository.getAllProducts(companyId).then(function (allProducts) {
             $scope.products = allProducts.data;
         });
 
@@ -15,7 +16,7 @@
             const newSupplier = {
                 Name: $scope.name,
                 Products: $scope.productsSelected,
-                CompanyId: loginRepository.getCompanyId()
+                CompanyId: companyId
         }
             suppliersRepository.addNewSupplier(newSupplier).then(function () {
                 $state.go('suppliers', {}, { reload: true });
