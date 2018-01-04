31089267a8f1d7cc6dcdd050c315b88830e1368a
Kreso <kreso@dump.hr>
2018-01-04 20:08:34 +0100

    Added timestamps to logs

--- WareMaster.Domain/Repositories/ActivityLogRepository.cs
+++ WareMaster.Domain/Repositories/ActivityLogRepository.cs
@@ -10,29 +10,25 @@ namespace WareMaster.Domain.Repositories
 {
     public class ActivityLogRepository
     {
-        public ActivityLogRepository()
-        {
-            _userRepository = new UserRepository();
-        }
-
-        private readonly UserRepository _userRepository;
-
         public List<ActivityLog> GetAllActivityLogs(int companyId)
         {
-            var userList = _userRepository.GetAllUsers(companyId).ToList();
-            var activityLogList = new List<ActivityLog>();
-
-            foreach (var user in userList)
-                    activityLogList.AddRange(user.ActivityLogs);
-
-            return activityLogList;
+            using (var context = new WarehouseContext())
+                return context.ActivityLogs.Where(log => log.CompanyId == companyId)
+                                           .OrderByDescending(order => order.TimeOfActivity)
+                                           .ToList();
         }
 
         public void AddActivityLog(ActivityLog activityLogToAdd)
         {
             using (var context = new WarehouseContext())
             {
+                activityLogToAdd.User = context.Users.Find(activityLogToAdd.UserId);
+                activityLogToAdd.Company = context.Companies.Find(activityLogToAdd.CompanyId);
+
                 context.Users.Attach(activityLogToAdd.User);
+                context.Companies.Attach(activityLogToAdd.Company);
+
+                activityLogToAdd.TimeOfActivity = DateTime.Now;
 
                 context.ActivityLogs.Add(activityLogToAdd);
                 context.SaveChanges();
