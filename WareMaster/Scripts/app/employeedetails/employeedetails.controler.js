f1f2112f6c43dedd36da6ac2f20721ee51a02c73
Ivan Vukorepa <ivukor00@fesb.hr>
2018-01-04 17:46:27 +0100

    created new files

---
+++ WareMaster/Scripts/app/employeedetails/employeedetails.controler.js
@@ -0,0 +1,7 @@
+﻿angular.module('app').controller('EmployeeDetailsController',
+    function ($scope, $state, $stateParams, employeesRepository) {
+
+        employeesRepository.getEmployeeToEdit($stateParams.id).then(function(employee) {
+            $scope.employee = employee.data;
+        });
+    });
---
+++ WareMaster/Scripts/app/employeedetails/employeedetails.state.js
@@ -0,0 +1,10 @@
+﻿angular.module('app').config(function ($stateProvider) {
+    $stateProvider
+        .state('employeedetails',
+            {
+                parent: 'employees',
+                url: '/details/:id',
+                controller: 'EmployeeDetailsController',
+                templateUrl: '/Scripts/app/employeedetails/employeedetails.template.html'
+            });
+});
