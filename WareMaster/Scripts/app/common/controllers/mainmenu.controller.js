44a37e1dba3dbf88cab28dac68f0ce4297997dc3
Kreso <kreso@dump.hr>
2018-01-04 01:42:23 +0100

    Refactored logout

---
+++ WareMaster/Scripts/app/common/controllers/mainmenu.controller.js
@@ -0,0 +1,10 @@
+﻿angular.module('app').controller('MainMenuController',
+    function ($scope, $state) {
+
+
+        $scope.logout = function() {
+            localStorage.removeItem('bearerToken');
+            localStorage.removeItem('authDetails');
+            $state.go('login');
+        }
+    });
