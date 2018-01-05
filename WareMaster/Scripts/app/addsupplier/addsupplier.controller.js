765aa1bc348efd419456ff142265fe891967a740
Kreso <kreso@dump.hr>
2018-01-05 13:43:49 +0100

    Fixing add supplier yet again

--- WareMaster/Scripts/app/addsupplier/addsupplier.controller.js
+++ WareMaster/Scripts/app/addsupplier/addsupplier.controller.js
@@ -1,8 +1,10 @@
 ﻿angular.module('app').controller('AddSupplierController',
-    function ($scope, $state, suppliersRepository, productsRepository) {
+    function ($scope, $state, suppliersRepository, productsRepository, loginRepository, activitylogRepository) {
         $scope.productsSelected = [];
 
-        productsRepository.getAllProducts().then(function (allProducts) {
+        const companyId = loginRepository.getCompanyId();
+
+        productsRepository.getAllProducts(companyId).then(function (allProducts) {
             $scope.products = allProducts.data;
         });
 
@@ -14,9 +16,15 @@
         $scope.addNewSupplier = function () {
             const newSupplier = {
                 Name: $scope.name,
-                Products: $scope.productsSelected
+                Products: $scope.productsSelected,
+                CompanyId: companyId
             }
             suppliersRepository.addNewSupplier(newSupplier).then(function () {
+                activitylogRepository.addActivityLog({
+                    Text: `${loginRepository.getManagerName()} je stvorio dobavljača ${$scope.name}`,
+                    UserId: loginRepository.getManagerId(),
+                    CompanyId: companyId
+                });
                 $state.go('suppliers', {}, { reload: true });
             });
         }
