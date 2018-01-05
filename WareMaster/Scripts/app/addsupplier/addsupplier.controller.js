e99272beb8bb8257292c4aa59cf92427a8cf50be
Kreso <kreso@dump.hr>
2018-01-05 01:14:20 +0100

    Readded supplier controller

--- WareMaster/Scripts/app/addsupplier/addsupplier.controller.js
+++ WareMaster/Scripts/app/addsupplier/addsupplier.controller.js
@@ -1,30 +1,22 @@
 ﻿angular.module('app').controller('AddSupplierController',
-    function ($scope, $state, suppliersRepository, productsRepository, loginRepository, activitylogRepository) {
+    function ($scope, $state, suppliersRepository, productsRepository) {
         $scope.productsSelected = [];
 
-        const companyId = loginRepository.getCompanyId();
-
-        productsRepository.getAllProducts(companyId).then(function (allProducts) {
+        productsRepository.getAllProducts().then(function (allProducts) {
             $scope.products = allProducts.data;
         });
 
-        $scope.selectProduct = function(product) {
+        $scope.selectProduct = function (product) {
             $scope.productsSelected.push(product);
-            $scope.products.splice($scope.products.indexOf(product),  1);
+            $scope.products.splice($scope.products.indexOf(product), 1);
         };
 
         $scope.addNewSupplier = function () {
             const newSupplier = {
                 Name: $scope.name,
-                Products: $scope.productsSelected,
-                CompanyId: companyId
-        }
+                Products: $scope.productsSelected
+            }
             suppliersRepository.addNewSupplier(newSupplier).then(function () {
-                activitylogRepository.addActivityLog({
-                    Text: `${loginRepository.getManagerName()} je stvorio dobavljača ${$scope.name}`,
-                    UserId: loginRepository.getManagerId(),
-                    CompanyId: companyId
-                });
                 $state.go('suppliers', {}, { reload: true });
             });
         }
