94db80e3a1f4384443e973227478a92696adda8f
Ivan Vukorepa <ivukor00@fesb.hr>
2018-01-05 11:03:38 +0100

    Finnished most of employee details, still to do: List out activity logs

--- WareMaster/Scripts/app/employeedetails/employeedetails.controler.js
+++ WareMaster/Scripts/app/employeedetails/employeedetails.controler.js
@@ -3,5 +3,16 @@
 
         employeesRepository.getEmployeeToEdit($stateParams.id).then(function(employee) {
             $scope.employee = employee.data;
+            $scope.orders = $scope.employee.EmployeeOrders;
+            $scope.chosenOrderFilter = "3";
         });
+
+        $scope.updateOrderList = function () {
+            if (parseInt($scope.chosenOrderFilter) === 3)
+                $scope.orders = $scope.employee.EmployeeOrders;
+            else
+                $scope.orders = $scope.employee.EmployeeOrders.filter(function(order) {
+                    return order.Status === parseInt($scope.chosenOrderFilter);
+                });
+        }
     });
